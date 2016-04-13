4071cb0fcddc4e2c0678e339a20642545f527baf
Jakobbjohansen <jajoh@byg.dtu.dk>
2016-04-13 10:27:50 +0200

    License update

--- Forskning med 1000 ord/Forskning med 1000 ord/About.Designer.cs
+++ Forskning med 1000 ord/Forskning med 1000 ord/About.Designer.cs
@@ -35,6 +35,7 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -62,7 +63,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(121, 134);
+            this.label3.Location = new System.Drawing.Point(121, 130);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(38, 13);
             this.label3.TabIndex = 2;
@@ -71,7 +72,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(85, 147);
+            this.label4.Location = new System.Drawing.Point(85, 144);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(110, 13);
             this.label4.TabIndex = 3;
@@ -80,7 +81,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(60, 160);
+            this.label5.Location = new System.Drawing.Point(66, 158);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(161, 13);
             this.label5.TabIndex = 4;
@@ -104,11 +105,21 @@
             this.label7.TabIndex = 6;
             this.label7.Text = "Released under GNU GPL v3";
             // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(100, 173);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(90, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "jajoh@byg.dtu.dk";
+            // 
             // About
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -135,5 +146,6 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
     }
 }
--- Forskning med 1000 ord/Forskning med 1000 ord/WordListDoesNotExist.cs
+++ Forskning med 1000 ord/Forskning med 1000 ord/WordListDoesNotExist.cs
@@ -1,4 +1,28 @@
-﻿using System;
+﻿/*  GNU General Public License
+  
+    Forskning med 1000 ord is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Forskning med 1000 ord is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Forskning med 1000 ord.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/*  Author:
+    Jakob Berg Johansen
+    Research Assistant at the Technical University of Denmark
+    jajoh@byg.dtu.dk
+    Date: 07-04-2016, Lyngby, Denmark
+ 
+    Copyright (c) 2016 Jakob Berg Johansen 
+    Released under GNU GPL v3
+*/
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
